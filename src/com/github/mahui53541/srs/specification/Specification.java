1d3ff18b9ebb7697feec24d9e5c3875d712439a3
mahui53541 <mahuiking9527@gmail.com>
2017-06-29 17:52:35 +0800

    重构

--- src/com/github/mahui53541/srs/specification/Specification.java
+++
@@ -1,8 +0,0 @@
-package com.github.mahui53541.srs.specification;
-
-import com.github.mahui53541.srs.daomain.Section;
-import com.github.mahui53541.srs.daomain.Student;
-
-public interface Specification<T> {
-	String validate(Student student, Section section);
-}
